2a094bb3d0521e2ed5e8d585e2d45ac2d11b5398
Nelson-Newton <Tumwesigenelson94@gmail.com>
2022-02-21 20:38:19 +0300

    Nelson-Newton

---
+++ chamber/js/discover.js
@@ -0,0 +1,36 @@
+//Getting images with data-src attribute
+const images = document.querySelectorAll("[data-src]");
+
+//For intersectional observation
+const imgOptions = {
+    threshold: 1,
+    rootMargin: "0px 0px 50px 0px"
+};
+
+const loadImages = (image) => {
+    image.setAttribute("src", image.getAttribute("data-src"));
+    image.onload = () => {
+        image.removeAttribute("data-src");
+    };
+};
+
+//for external intersection observer is supported
+if ("intersectionObserver" in window) {
+    const imgObserver = new IntersectionObserver((items,
+        imgObserver) => {
+        items.forEach(item => {
+            if (!item.isIntersecting) {
+                return;
+            } else {
+                loadImages(item.target);
+                imgObserver.unobserve(item.target);
+
+            }
+        });
+    }, imgOptions);
+
+    images.forEach(image => {
+        imgObserver.observe(image);
+
+    });
+}
